c81ac80cfeb12ede4e160cb6166c2aaeec628ab9
Michelle Lee <lee.michellejung@gmail.com>
2017-02-23 18:32:38 -0800

    presidents can be deleted and "uncovered" successfully now!

--- angular_http/starter_code/public/controllers/presidentsController.js
+++ angular_http/starter_code/public/controllers/presidentsController.js
@@ -5,22 +5,71 @@
     .module('ThePresidentsApp')
     .controller('PresidentsController', PresidentsController);
 
-  PresidentsController.$inject = [];
+  PresidentsController.$inject = ['$http'];
 
-  function PresidentsController(){
+  function PresidentsController($http){
     var vm = this;
-    vm.all = [
-      {"name": "Blorp Florp McRichards", "start": 1789, "end": 1790 },
-      {"name": "John MuscleBrain Adams", "start": 1790, "end": 1801 },
-      {"name": "Blogpost Dorgabn", "start": 1801, "end": 1949 },
-      {"name": "Mike", "start": 1949, "end": 1947 }
-    ];
+    // vm.all = [
+    //   {"name": "Blorp Florp McRichards", "start": 1789, "end": 1790 },
+    //   {"name": "John MuscleBrain Adams", "start": 1790, "end": 1801 },
+    //   {"name": "Blogpost Dorgabn", "start": 1801, "end": 1949 },
+    //   {"name": "Mike", "start": 1949, "end": 1947 }
+    // ];
     vm.addPresident = addPresident;
+    vm.deletePresident = deletePresident;
+    vm.uncoverPresident = uncoverPresident;
     vm.newPresident = {};
+    vm.all = [];
+
+    getPresidents();
+
+    function getPresidents(){
+      $http
+      .get('/api/presidents')
+      .then(function(response){
+        vm.all = response.data.presidents;
+      }, function(err) {
+          console.log(err);
+      });
+    }
 
     function addPresident(){
-      vm.all.push(vm.newPresident);
-      vm.newPresident = {};
+      $http
+        .post('/api/presidents', vm.newPresident)
+        .then(function(res) {
+          vm.all.push(res.data.president);
+          vm.newPresident = {};
+        }, function(err) {
+          console.log(err);
+        });
     }
-  }
+
+    function deletePresident(prez){
+      var id = prez._id;
+      let url = "api/presidents/".concat(id);
+      $http
+        .delete(url)
+        .then(function(res) {
+          getPresidents()
+        }, function(err) {
+          console.log(err);
+        });
+    }
+
+
+    function uncoverPresident(prez){
+      let url = "api/presidents/".concat(prez._id);
+      let uncover = {uncovered: !prez.uncovered};
+      $http
+        .put(url, uncover)
+        .then(function(res){
+          getPresidents()
+        }, function(err) {
+          console.log(err);
+        })
+    }
+
+    }
+
+
 })();
